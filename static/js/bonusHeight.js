7990e80a68ad44592679c7c4a1db980e54c39231
marinm309 <marinm309@abv.bg>
2022-12-29 17:59:21 +0200

    done

--- static/js/bonusHeight.js
+++ static/js/bonusHeight.js
@@ -1,7 +1,7 @@
 const inputs = document.querySelectorAll('input')
 	inputs[inputs.length - 1].addEventListener('focus', function(){
-		document.body.style.height = '150vh'
-		document.querySelector('.bg').style.height = '150vh'
+		document.body.style.height = '210vh'
+		document.querySelector('.bg').style.height = '210vh'
 	})
 	inputs[inputs.length - 1].addEventListener('focusout', function(){
 		document.body.style.height = '100vh'
