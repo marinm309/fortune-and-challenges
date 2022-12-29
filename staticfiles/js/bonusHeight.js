b7a1a31c27a28d6822692942a2aa9d8adf3c27af
marinm309 <marinm309@abv.bg>
2022-12-29 18:16:41 +0200

    done

--- staticfiles/js/bonusHeight.js
+++ staticfiles/js/bonusHeight.js
@@ -1,5 +1,6 @@
 const inputs = document.querySelectorAll('input')
 const arr = [].slice.call(inputs)
+let cnt = 0
 
 arr.forEach((x) => {
 	x.addEventListener('focus', increase)
@@ -7,7 +8,6 @@ arr.forEach((x) => {
 })
 
 function increase() {
-	let cnt = 0
 	if (cnt > 0) {
 		document.body.style.height = '210vh'
 		document.querySelector('.bg').style.height = '210vh'
