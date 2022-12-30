fa773692bc37ff500ff7bff53f4ae6a3c2c38a6d
marinm309 <marinm309@abv.bg>
2022-12-30 11:37:57 +0200

    done

--- static/js/bonusHeight.js
+++ static/js/bonusHeight.js
@@ -1,6 +1,11 @@
 const inputs = document.querySelectorAll('input')
 const arr = [].slice.call(inputs)
 let cnt = 0
+let currentSize
+
+window.addEventListener('resize', function () {
+	currentSize = this.document.body.clientWidth
+})
 
 arr.forEach((x) => {
 	x.addEventListener('focus', increase)
@@ -8,11 +13,13 @@ arr.forEach((x) => {
 })
 
 function increase() {
-	if (cnt > 0) {
-		document.body.style.height = '210vh'
-		document.querySelector('.bg').style.height = '210vh'
+	if (currentSize <= 800) {
+		if (cnt > 0) {
+			document.body.style.height = '210vh'
+			document.querySelector('.bg').style.height = '210vh'
+		}
+		cnt++
 	}
-	cnt++
 }
 
 function decrease() {
